1e77a39070326d26fe65d9893bc9e8d0ec7137c9
btirth92650 <bhalanitirth999@gmail.com>
2022-11-18 12:30:40 +0530

    prime ng add

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,42 @@ import { ThmHeaderComponent } from './theme/thm-header/thm-header.component';
 import { ThmMenubarComponent } from './theme/thm-menubar/thm-menubar.component';
 import { RouterModule } from '@angular/router';
 
+/**************** Prime-NG Content Import Here ******************/
+
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import { CalendarModule } from 'primeng/calendar';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {ProgressBarModule} from 'primeng/progressbar'
+import {InputSwitchModule} from 'primeng/inputswitch';
+
+const PRIME_NG = [
+  CalendarModule,
+  DropdownModule,
+  TableModule,
+  ToastModule,
+  ConfirmDialogModule,
+  MultiSelectModule,
+  ButtonModule,
+  TableModule,
+  ProgressSpinnerModule,
+  RadioButtonModule,
+  AutoCompleteModule,
+  SelectButtonModule,
+  ProgressBarModule,
+  InputSwitchModule,
+  // ScrollPanelModule,
+ 
+]
 
 
 @NgModule({
@@ -16,14 +52,16 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    PRIME_NG
   ],
-  
+  providers: [MessageService,PrimeNGConfig,ConfirmationService],
   exports:[
     FormsModule,
     ReactiveFormsModule,
     ThmHeaderComponent,
-    ThmMenubarComponent
+    ThmMenubarComponent,
+    PRIME_NG
   ]
 })
 export class SharedModule { }
